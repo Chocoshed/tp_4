78b9a782241f38d8b1d8b413b7598680ad48e9b0
Julie Fromageat <julie.fr6799@gmail.com>
2025-10-30 15:52:25 +0100

    add credential

--- api/config.js
+++ api/config.js
@@ -1,11 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mva0dl3ps73dimggg-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_user",
+    "password" : "BicnjBbyuzuxsDY7Lvza9OrVEP91fO23",
+    "bdname" :"pollution" 
     }
 }
-
