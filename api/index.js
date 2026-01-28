787dd38628bca3c7db2ea87f2d01fcb46573c623
Julie Fromageat <julie.fr6799@gmail.com>
2026-01-28 20:17:53 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const app  = express ();
 
 var corsOptions = {
-  origin: "https://tp04-fromageat-julie.onrender.com/",
+  origin: "*",
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   headers: 'Content-Type, Authorization',
